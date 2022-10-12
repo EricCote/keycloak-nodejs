664ee4a6ad739e656fa451d3020d79a8fdcdd6b1
Eric Cote <eric@coteexpert>
2022-10-12 02:56:56 -0400

    update references

--- config/keycloak-config.js
+++ config/keycloak-config.js
@@ -1,36 +1,36 @@
-
-const Keycloak = require('keycloak-connect');
+const Keycloak = require("keycloak-connect");
 
 let _keycloak;
 
 var keycloakConfig = {
-    serverUrl: "https://keycloak2.reactacademy.live/",
-    realm: "react-courses",
-    clientId: "august-course",
-    realmPublicKey: "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApBAvNIr2JlndzYxCS4/zbr2CEdKtkpfpo3N/6I/150t7ZrWSdDJl0Jr7Bb8215npCG/FzuiP5mLnsslPkP7JUbALJtEduNrQ0mCyrUeLdXoEtuXeLI0rqxsSRxioZED98uA71N1UrsF6I/a58U+cFJqeZ92+zF/a2Ur3MltoT193la0XvxkjFf07u7O92a1EqBCfgwSw1dGpPtK4NMHZCgezKGQXsyTewHapZxz5JDc2gDKOBU9PNOvg1HOCZiEflOCBnceVbEwMRUxtbh0H/3n/Y8B4V4stQhFlUgCKB7p5MGtMPYp5aGVjptCFNVbLse/2qxSlBt2ZAAuE7oIKFwIDAQAB",
-    bearerOnly: true,
-   
+  serverUrl: "https://keycloak2.reactacademy.live/",
+  realm: "react-courses",
+  clientId: "august-course",
+  realmPublicKey:
+    "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlgE18nhFEu2S2b4O5GXq4Wk1d1YxSnhmFHwt9F77gCgqEZkeNNWSghBYwybyNuisIDsFwccJzaOQjUj9Trq51S0u6EtKtXlhNd5R8GjIkj7aUJ30bsruGwISt8EAbNhxdDq8ODNuDpM0ONENDFuPgivLHcBAFSeap4MviW2qmPZv4DXdUVUbK/1/zM2VuURM+8xzEa92BbZhHZJM49r4nIi4aprtXIW+KDs301P35WY7CXIO4tebaX2voPt7jIFhVCOaoQyTeM+eO6Co0IJggQqOj6M/2vfW7ipBxRHmw76fOjlpm8SMj7eMtHKyJxGpskliF5VG01it69sKm+7XgwIDAQAB",
+  bearerOnly: true,
 };
 
 function initKeycloak(memoryStore) {
-    if (_keycloak) {
-        console.warn("Trying to init Keycloak again!");
-        return _keycloak;
-    } 
-    else {
-        _keycloak = new Keycloak({ store: memoryStore }, keycloakConfig);
-        return _keycloak;
-    }
+  if (_keycloak) {
+    console.warn("Trying to init Keycloak again!");
+    return _keycloak;
+  } else {
+    _keycloak = new Keycloak({ store: memoryStore }, keycloakConfig);
+    return _keycloak;
+  }
 }
 
 function getKeycloak() {
-    if (!_keycloak){
-        console.error('Keycloak has not been initialized. Please called init first.');
-    } 
-    return _keycloak;
+  if (!_keycloak) {
+    console.error(
+      "Keycloak has not been initialized. Please called init first."
+    );
+  }
+  return _keycloak;
 }
 
 module.exports = {
-    initKeycloak,
-    getKeycloak
+  initKeycloak,
+  getKeycloak,
 };
